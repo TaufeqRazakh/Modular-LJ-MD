3d3b4c3d16d79f031d1eabfd3ec019e68d1d2eb0
Taufeq  Razakh <>
2019-12-30 00:53:41 -0800

    Compute_accel to be called from outside

--- pmd.cpp
+++ pmd.cpp
@@ -45,36 +45,33 @@ public:
 };
  
 
-class Cell {
+class SubSystem {
 public:
 
   int pid; //sequential processor ID of this cell
 
-  array<int, 3> al; // Box length per processor
-  array<int, 3> vid; /* Vector index of this processor */
-  array<int, 3> myparity; // Parity of this processor
+  array<int, 3> myParity; // Parity of this processor
   array<int, 6> nn; // Neighbor node list of this processor
+  vector<int> cellList; // Scalar cell index list for the subsystem
   vector<vector<double> > sv; // Shift vector to the 6 neighbors
   array<double, 3> vSum, gvSum;
-  vector<Atom> atoms;
+  vector<Atom> systemAtoms; // all atoms within the processor
   int n; // Number of resident atoms in this processor
   double comt; // elapsed wall clock time & Communication time in second
   
   /* Create cell with by taking the parameters for InitUcell and InitTemp 
      we calculate the number of atoms and give them random velocities */
-  Cell(array<int, 3> vproc, array<int, 3> InitUcell, double InitTemp, double Density) {
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+  SubSystem(array<int, 6> neighborNode, array<int, 3> parityTable, sid){
+    nn = neighborNode;
+    myParity = parityTable;
+    pid = sid;
 
     default_random_engine generator;
     normal_distribution<double> distribution(1,1);
-
+    
     /* Compute basic parameters */
     for (int i=0; i<3; i++) al[i] = InitUcell[i]/cbrt(Density/4.0);
     if (pid == 0) printf("al = %e %e %e\n",al[0],al[1],al[2]);
-  
-    // Prepare the Neighbot-node table
-    InitNeighborNode(vproc);
 
     // Initialize lattice positions and assign random velocities
     array<double, 3> c,gap;
@@ -101,7 +98,7 @@ public:
 	    atom.vy = sqrt(3*InitTemp)*distribution(generator);
 	    atom.z = c[2] + gap[2]*origAtom[j][2];
 	    atom.vz = sqrt(3*InitTemp)*drand48();
-	    atoms.push_back(atom);
+	    systemAtoms.push_back(atom);
 
 	    vSum[0] += atom.vx;
 	    vSum[1] += atom.vy;
@@ -123,40 +120,6 @@ public:
       atom.vx -= gvSum[0];
       atom.vy -= gvSum[1];
       atom.vz -= gvSum[2];
-    }
-  }
-
-  void InitNeighborNode(array<int, 3> vproc) {
-    // Prepare neighbor-node ID table for a cell
-    vid[0] = pid/(vproc[1]*vproc[2]);
-    vid[1] = (pid/vproc[2])%vproc[1];
-    vid[2] = pid%vproc[2];
-    
-    vector<vector<int > > iv = {
-				{-1,0,0}, {1,0,0}, {0,-1,0}, {0,1,0}, {0,0,-1}, {0,0,1}
-    };
-    
-    int ku, a, k1[3];
-    
-    /* Set up neighbor tables, nn & sv */
-    for (ku=0; ku<6; ku++) {
-      /* Vector index of neighbor ku */
-      for (a=0; a<3; a++)
-	k1[a] = (vid[a]+iv[ku][a]+vproc[a])%vproc[a];
-      /* Scalar neighbor ID, nn */
-      nn[ku] = k1[0]*vproc[1]*vproc[2]+k1[1]*vproc[2]+k1[2];
-      /* Shift vector, sv */
-      for (a=0; a<3; a++) sv[ku][a] = al[a]*iv[ku][a];
-    }
-    
-    // Set up node parity table
-    for (a=0; a<3; a++) {
-      if (vproc[a] == 1) 
-	myparity[a] = 2;
-      else if (vid[a]%2 == 0)
-	myparity[a] = 0;
-      else
-	myparity[a] = 1;
     }
   }
 
@@ -444,20 +407,14 @@ int main(int argc, char **argv) {
 
   // vproc - number of processrs in x|y|z directions
   // Initucell - Number of unit cells per processor
-  array<int, 3> vproc, InitUcell; 
-  // init_params(); This is brought into the main function
-
   // Density - Density of atoms
   // InitTemp - Starting temperature
   // DeltaT - Size of time step
   // StepLimit - Number of time steps to be simulated
   // StepAvg - Reporting interval forces statistical data
+  array<int, 3> vproc, InitUcell; 
   double Density, InitTemp, DeltaT;
-  int StepLimit, StepAvg;
-  
-  double rr,ri2,ri6,r1;
-  double Uc, Duc;
-  
+  int StepLimit, StepAvg;  
   
   /* Read control parameters */
   ifstream ifs("pmd.in", ifstream::in);
@@ -472,28 +429,84 @@ int main(int argc, char **argv) {
   ifs >> StepLimit >> StepAvg;
 
   ifs.close();
+
+  array<int, 3> vid{}; /* Vector index of this processor */
+  vid[0] = pid/(vproc[1]*vproc[2]);
+  vid[1] = (pid/vproc[2])%vproc[1];
+  vid[2] = pid%vproc[2];
   
-  /* Constants for potential truncation */
-  rr = RCUT*RCUT; ri2 = 1.0/rr; ri6 = ri2*ri2*ri2; r1=sqrt(rr);
-  Uc = 4.0*ri6*(ri6 - 1.0);
-  Duc = -48.0*ri6*(ri6 - 0.5)/r1;
+  /* Compute basic parameters */
+  Double DeltaTH = 0.5*DeltaT; // Half the time step
 
-  // set_topology(); This is now implemented in the Cell object. Each cell makes its neighbor tables
-  // init_conf(); This is now implemented within the Cell constructor
+  int a; //iterator variable
 
-  atom_copy();
-  compute_accel(); /* Computes initial accelerations */ 
+  array<int, 3> al{}; // Box length per processor since each subsystem is a parallel-piped
+  for (a=0; a<al.size(); a++) al[a] = InitUcell[a]/cbrt(Density/4.0);
+
+  array<int ,3> lc{}, rc{};
+  /* Compute the # of cells for linked cell lists */
+  for (a=0; a<3; a++) {
+    lc[a] = al[a]/RCUT; 
+    rc[a] = al[a]/lc[a];
+  }
+
+  if(sid == 0) {
+    cout << "al = " << al[0] << al[1] << al[2] << endl;
+    cout << "lc = " << lc[0] << lc[1] << lc[2] << endl;
+    cout << "rc = " << rc[0] << rc[1] << rc[2] << end;
+  }
+
+  array<int, 3> nn{}, myParity{};
+  
+  SetTopology(nn, myParity, vid, vproc); // Uses reference to fill up nn and myparity
+
+  // This section emulates init_conf and sets up cells in the processor
+  SubSystem s;
+
+  s.AtomCopy();
+  compute_accel(s);
 
   cpu1 = MPI_Wtime();
   for (stepCount=1; stepCount<=StepLimit; stepCount++) {
     single_step(); 
     if (stepCount%StepAvg == 0) eval_props();
   }
+  
   cpu = MPI_Wtime() - cpu1;
   if (sid == 0) printf("CPU & COMT = %le %le\n",cpu,comt);
 
   MPI_Finalize(); /* Clean up the MPI environment */
   return 0;
+}
+
+void SetTopology(array<int, 3> &nn, array<int, 3> &myParity, array<int, 3> vid, array<int, 3> vproc) {
+  // Prepare neighbor-node ID table for a cell        
+  vector<vector<int > > iv = {
+			      {-1,0,0}, {1,0,0}, {0,-1,0}, {0,1,0}, {0,0,-1}, {0,0,1}
+  };
+  
+  int ku, a, k1[3];
+  
+  /* Set up neighbor tables, nn & sv */
+  for (ku=0; ku<6; ku++) {
+    /* Vector index of neighbor ku */
+    for (a=0; a<3; a++)
+      k1[a] = (vid[a]+iv[ku][a]+vproc[a])%vproc[a];
+    /* Scalar neighbor ID, nn */
+    nn[ku] = k1[0]*vproc[1]*vproc[2]+k1[1]*vproc[2]+k1[2];
+    /* Shift vector, sv */
+    for (a=0; a<3; a++) sv[ku][a] = al[a]*iv[ku][a];
+  }
+  
+  // Set up node parity table
+  for (a=0; a<3; a++) {
+    if (vproc[a] == 1) 
+      myparity[a] = 2;
+    else if (vid[a]%2 == 0)
+      myparity[a] = 0;
+    else
+      myparity[a] = 1;
+  }
 }
 
 /*--------------------------------------------------------------------*/
@@ -513,19 +526,31 @@ r & rv are propagated by DeltaT using the velocity-Verlet scheme.
 }
 
 /*--------------------------------------------------------------------*/
-void compute_accel() {
+void compute_accel(SubSystem &s) {
 /*----------------------------------------------------------------------
 Given atomic coordinates, r[0:n+nb-1][], for the extended (i.e., 
 resident & copied) system, computes the acceleration, ra[0:n-1][], for 
 the residents.
 ----------------------------------------------------------------------*/
-  int i,j,a,lc2[3],lcyz2,lcxyz2,mc[3],c,mc1[3],c1;
   int bintra;
   double dr[3],rr,ri2,ri6,r1,rrCut,fcVal,f,vVal,lpe;
 
+  double rr,ri2,ri6,r1;
+  double Uc, Duc;
+
+  vector<Atom>::iterator it_atom;
+  
+  /* Constants for potential truncation */
+  rr = RCUT*RCUT; ri2 = 1.0/rr; ri6 = ri2*ri2*ri2; r1=sqrt(rr);
+  Uc = 4.0*ri6*(ri6 - 1.0);
+  Duc = -48.0*ri6*(ri6 - 0.5)/r1;
+  
   /* Reset the potential & forces */
-  lpe = 0.0;
-  for (i=0; i<n; i++) for (a=0; a<3; a++) ra[i][a] = 0.0;
+  for(it_atom = s.systemAtoms.begin(); it_atom != cell.atoms.end(); ++it_atom) {
+    it_atom->ax = 0.0;
+    it_atom->ay = 0.0;
+    it_atom->az = 0.0;
+  }
 
   /* Make a linked-cell list, lscl------------------------------------*/
 
@@ -551,6 +576,7 @@ the residents.
     head[c] = i;
   } /* Endfor atom i */
 
+
   /* Calculate pair interaction---------------------------------------*/
 
   rrCut = RCUT*RCUT;
@@ -563,7 +589,7 @@ the residents.
     /* Calculate a scalar cell index */
     c = mc[0]*lcyz2+mc[1]*lc2[2]+mc[2];
     /* Skip this cell if empty */
-    if (head[c] == EMPTY) continue;
+    if (head[c] == -1) continue;
 
     /* Scan the neighbor cells (including itself) of cell c */
     for (mc1[0]=mc[0]-1; mc1[0]<=mc[0]+1; (mc1[0])++)
@@ -573,15 +599,15 @@ the residents.
       /* Calculate the scalar cell index of the neighbor cell */
       c1 = mc1[0]*lcyz2+mc1[1]*lc2[2]+mc1[2];
       /* Skip this neighbor cell if empty */
-      if (head[c1] == EMPTY) continue;
+      if (head[c1] == -1) continue;
 
       /* Scan atom i in cell c */
       i = head[c];
-      while (i != EMPTY) {
+      while (i != -1) {
 
         /* Scan atom j in cell c1 */
         j = head[c1];
-        while (j != EMPTY) {
+        while (j != -1) {
 
           /* No calculation with itself */
           if (j != i) {
